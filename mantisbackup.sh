daf5293a005bdb37f0f233c74bdfece97b16ce14
Patrick Laxton <patrick.alxton@gmail.com>
2014-11-20 12:29:53 +0100

    Sends the file to the FTP server

--- mantisbackup.sh
+++ mantisbackup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # File variables
-TIMESTAMP=`which date`' +%s'
-CURRENTTIMESTAMP=`$TIMESTAMP`
-FILENAME='/tmp/mantisbt.'$CURRENTTIMESTAMP'.sql'
+DATETIME=`which date`' +%Y%m%d.%H%M%S'
+CURRENTDATETIME=`$DATETIME`
+FILENAME='mantisbt.'$CURRENTDATETIME'.sql'
+LOCALFILEPATH='/tmp/'$FILENAME
 
 # MySQL variables
 MYSQLDUMP=`which mysqldump`
@@ -12,5 +13,17 @@ MYSQLUSER='root'
 MYSQLPASS='XXXX'
 MYSQLDB='mantisbt'
 
+# FTP variables
+FTP=`which ftp`
+FTPHOST='XXX.XXX.XXX.XXX'
+FTPUSER='XXXX'
+FTPPASS='XXXX'
+DISTANTFILEPATH='~/'$FILENAME
+
+ECHO=`which echo`
+
 # Dumps MySQL Database into /tmp
-$MYSQLDUMP -h $MYSQLHOST -u $MYSQLUSER --password=$MYSQLPASS -d $MYSQLDB > $FILENAME
+$MYSQLDUMP -h $MYSQLHOST -u $MYSQLUSER --password=$MYSQLPASS -d $MYSQLDB > $LOCALFILEPATH
+
+# Sends the file using FTP
+$ECHO -e "open $FTPHOST\nuser $FTPUSER $FTPPASS\nput $LOCALFILEPATH $DISTANTFILEPATH\nbye" | $FTP -n
