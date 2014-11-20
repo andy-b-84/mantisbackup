104721e61a6d972312250c0d604765adcb1aa2e2
Patrick Laxton <patrick.alxton@gmail.com>
2014-11-20 12:38:54 +0100

    deletes the temporary file, commented a little bit more

--- mantisbackup.sh
+++ mantisbackup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Will dump the mantisbt database in a local file in /tmp,
+# then put it in the /home folder of a FTP user on its server,
+# then delete the local file.
+#
+# You have to provide correct values below to make this script do anyhting.
+# Please feel free to change any value which does not reflect your configuration.
+# 
+# Two ways to call it :
+#    chmod 755 mantisbackup.sh;# or chmod u+x, but I hate that syntax
+#    then type ./mantisbackup.sh
+# or just type :
+#    /bin/bash ./mantisbackup.sh
+
 # File variables
 DATETIME=`which date`' +%Y%m%d.%H%M%S'
 CURRENTDATETIME=`$DATETIME`
@@ -10,20 +23,25 @@ LOCALFILEPATH='/tmp/'$FILENAME
 MYSQLDUMP=`which mysqldump`
 MYSQLHOST='localhost'
 MYSQLUSER='root'
-MYSQLPASS='XXXX'
+MYSQLPASS='PUT_YOUR_MYSQL_PASSWORD_HERE'
 MYSQLDB='mantisbt'
 
 # FTP variables
 FTP=`which ftp`
-FTPHOST='XXX.XXX.XXX.XXX'
-FTPUSER='XXXX'
-FTPPASS='XXXX'
+FTPHOST='PUT_YOUR_FTP_HOST_HERE'
+FTPUSER='PUT_YOUR_FTP_USER_HERE'
+FTPPASS='PUT_YOUR_FTP_PASSWORD_HERE'
 DISTANTFILEPATH='~/'$FILENAME
 
+# Basic System Programs
 ECHO=`which echo`
+RM=`which rm`
 
 # Dumps MySQL Database into /tmp
 $MYSQLDUMP -h $MYSQLHOST -u $MYSQLUSER --password=$MYSQLPASS -d $MYSQLDB > $LOCALFILEPATH
 
 # Sends the file using FTP
 $ECHO -e "open $FTPHOST\nuser $FTPUSER $FTPPASS\nput $LOCALFILEPATH $DISTANTFILEPATH\nbye" | $FTP -n
+
+# Deletes the local file
+$RM $LOCALFILEPATH
